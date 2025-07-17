2395e6f1acbcdc17b3d2d23c9368f3b90a4fc2ed
abhinavoo7 <gabhinav95a@gmail.com>
2025-07-18 00:19:54 +0530

    added cloudinary config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      throw new Error("No file path provided for upload.");
+    }
+    // Upload an image
+    const uploadResult = await cloudinary.uploader
+      .upload(localFilePath, {
+        resource_type: "auto", // Automatically determine the resource type
+      })
+      .then(() => {
+        console.log("File uploaded successfully to Cloudinary.");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(uploadResult);
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // Delete the file if upload fails
+    console.error("Error uploading file to Cloudinary:", error);
+    return null;
+  }
+};
