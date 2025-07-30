e15740f9201acbf5baef0c0cb5ec4b67001b57b8
abhinavoo7 <gabhinav95a@gmail.com>
2025-07-31 00:32:50 +0530

    added login and logout routes

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 
-export const verifyJwt = asyncHandler(async (req, res, next) => {
+export const verifyJwt = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
